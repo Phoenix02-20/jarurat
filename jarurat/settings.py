816b1cfdc8c7e0904f2862e47afefea96c3ef02c
Phoenix02-20 <78334594+Phoenix02-20@users.noreply.github.com>
2022-03-18 21:43:09 +0530

    Update settings.py
    commit save

--- jarurat/settings.py
+++ jarurat/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ow&0z8ci9$yq7loc3#jd=v9ofm*b1mo=^q!f=s4%&p)z76c*0w'
+SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
